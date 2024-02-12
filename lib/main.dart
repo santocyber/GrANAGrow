03fc1444c10c1376afeb4c07fad1419c332bf313
SantoCyber <santocyber@gmail.com>
2024-02-12 12:51:19 -0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:webview_flutter/webview_flutter.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 void main() {
   runApp(MyApp());
@@ -9,7 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Exibir Página PHP',
+      title: 'Consulta PostgreSQL',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
@@ -24,15 +25,47 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  String data = '';
+
+  Future<void> fetchData() async {
+    final response = await http.get(Uri.parse('SUA_URL_AQUI'));
+    if (response.statusCode == 200) {
+      setState(() {
+        data = json.decode(response.body)['data'];
+      });
+    } else {
+      setState(() {
+        data = 'Erro ao carregar dados';
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Exibir Página PHP'),
+        title: Text('Consulta PostgreSQL'),
       ),
-      body: WebView(
-        initialUrl: 'https://santocyber.helioho.st/granagw/',
-        javascriptMode: JavascriptMode.unrestricted,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            ElevatedButton(
+              onPressed: fetchData,
+              child: Text('Buscar Informação'),
+            ),
+            SizedBox(height: 20),
+            Text(
+              'Informação do Banco de Dados:',
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 10),
+            Text(
+              data,
+              style: TextStyle(fontSize: 16),
+            ),
+          ],
+        ),
       ),
     );
   }
