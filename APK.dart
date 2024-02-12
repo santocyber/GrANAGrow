e0da81c8b652e8b409b4d424312dc53e3f953ba5
SantoCyber <santocyber@gmail.com>
2024-02-12 12:01:20 -0300

    Update APK.dart

--- APK.dart
+++ APK.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
+import 'package:webview_flutter/webview_flutter.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,7 +9,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'PHP Connection Demo',
+      title: 'Exibir Página PHP',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
       home: MyHomePage(),
     );
   }
@@ -21,45 +24,15 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  String _response = '';
-
-  Future<void> _fetchData() async {
-    try {
-      final response = await http.get(Uri.parse('https://santocyber.helioho.st/granagw/'));
-      if (response.statusCode == 200) {
-        setState(() {
-          _response = response.body;
-        });
-      } else {
-        setState(() {
-          _response = 'Erro: ${response.statusCode}';
-        });
-      }
-    } catch (error) {
-      setState(() {
-        _response = 'Erro: $error';
-      });
-    }
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('PHP Connection Demo'),
+        title: Text('Exibir Página PHP'),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            ElevatedButton(
-              onPressed: _fetchData,
-              child: Text('Conectar à Página PHP'),
-            ),
-            SizedBox(height: 20),
-            Text(_response),
-          ],
-        ),
+      body: WebView(
+        initialUrl: 'https://santocyber.helioho.st/granagw/',
+        javascriptMode: JavascriptMode.unrestricted,
       ),
     );
   }
