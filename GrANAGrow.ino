bd6757ab312ee5f5dfb7d4db926a1e0d4c67b7c8
SantoCyber <santocyber@gmail.com>
2024-02-22 18:48:41 -0300

    Update GrANAGrow.ino

--- GrANAGrow.ino
+++ GrANAGrow.ino
@@ -1,55 +1,80 @@
+//###CONFIG INCIAIS ATIVACAO DE MODULOS
+
+#define SENSORES 1
+#define CAMERA 0
+
+
 //#Bibliotecas REDE
 #include <WiFi.h>
-#include <WebServer.h>
 #include <ESPmDNS.h>
 #include <HTTPClient.h>
 #include <ESP32Ping.h>
 
+// Crie um objeto client
+WiFiClientSecure client;
 
-#define SENSORES 1
-#include <Wire.h>  // Incluindo a biblioteca do barramento I2C
-#include <Adafruit_BMP280.h>  // Incluindo a biblioteca do sensor de pressão BMP280
-#include <Adafruit_AHTX0.h>   // Incluindo a biblioteca do sensor de temperatura e umidade AHTX0
+//#Biblioteca WEBSERVER
+#include <WebServer.h>
 
+// Cria um objeto WebServer
+WebServer server(80);
+
+//#Biblioteca SENSORES
+#if (SENSORES == 1)
+
+#include <Adafruit_BMP280.h>  
+#include <Adafruit_AHTX0.h> 
+#include <Arduino.h>
+#include "Wire.h"
 
 Adafruit_BMP280 bmp;  // Objeto do sensor BMP280
 Adafruit_AHTX0 aht;   // Objeto do sensor AHTX0
+#define BMP280_ADDRESS 0x77
+#define AHT_ADDRESS 0x38
+
+Adafruit_Sensor *aht_humidity, *aht_temp;
+Adafruit_Sensor *bmp_pressure = bmp.getPressureSensor();
+
+#endif
+
+//#define I2C_MASTER_SCL1 18
+//#define I2C_MASTER_SDA1 17
+
+//#define I2C_MASTER_SCL1 22
+//#define I2C_MASTER_SDA1 21
+
+//#define I2C_MASTER_SCL1 5 // Exemplo de pino para a segunda porta I2C
+//#define I2C_MASTER_SDA1 4 // Exemplo de pino para a segunda porta I2C
 
 
-// Configuração do certificado CA (Certificado de Autoridade) para confiar no servidor remoto
-const char* caCert = \
- "-----BEGIN CERTIFICATE-----\n"
-  "MIICtjCCAZ4CAQAwFTETMBEGA1UEAwwKdGVzLm9ibS5wdDCCASIwDQYJKoZIhvcN\n"
-  "AQEBBQADggEPADCCAQoCggEBANvuYarL3NB+2S4uGc8IftHLctFT8qKAzAcJt3yJ\n"
-  "iFa4dixMgxNMnNZ8t1iAMEIlm0exzQ9Jbhi0i27RGwXnUDU5sS9RESqc02ca3HSd\n"
-  "DBWCc3RYxetHJTJWAIam7D7qlsHTdLBAKUKKhytHD+uFYcC0D44IHfUmXmoQva9c\n"
-  "QNSWeFZJQOywcclQyaThjSpg9xbg09Nx1/JLRV4ocKMnSm1oR9EkGu1oI8ppgeQv\n"
-  "wrEyPYniRlNY0uT7K+NvCKZ46J1naPrnWwleqxp/JGfykH63u6s4VLZEQ7ibmAI3\n"
-  "WX3hXTcGYxcz3LvwZNjpRslAX3iFLr9docf0HjSbF70zcv0CAwEAAaBcMFoGCSqG\n"
-  "SIb3DQEJDjFNMEswCQYDVR0TBAIwADALBgNVHQ8EBAMCBeAwMQYDVR0RBCowKIIK\n"
-  "dGVzLm9ibS5wdIIMKi50ZXMub2JtLnB0ggwqLnRlcy5vYm0ucHQwDQYJKoZIhvcN\n"
-  "AQELBQADggEBALYRG8PoLow3R6Z7X9ZntRvHYksRhaUsgmOqgqB/H2SjEFZ0kTvY\n"
-  "0BIG98FFfHRG8omo/jlsPBBD/el8WzrBNRWvennpvWRteBheM5eSruHjZO5tOwHd\n"
-  "b+A6i8GmMpQVAzx31oM+fNXUzUDUGtltIxxIQMsclkqiGWuT56QZ2oZEtVTOmQP9\n"
-  "NPsmirCe0CfN47LgHz73X1wjp6nd09yloR/pFO4H+gruyh+NwnKp9wP4Pq/3i6g7\n"
-  "hl0mVBvlvMs/7Jt3guvcdSHPc2yyEiarRUPUxCzrjG8RRr7HpY7xkKYRI06ROPsj\n"
-  "LOYRaRnaPslLfcSXH1MsIjXRgq077tzeHdo=\n"
-  "-----END CERTIFICATE-----\n";
 
+// Configurao do certificado CA (Certificado de Autoridade) para confiar no servidor remoto
+const char* caCert= \
+     "-----BEGIN CERTIFICATE-----\n" \
+     "MIIDSjCCAjKgAwIBAgIQRK+wgNajJ7qJMDmGLvhAazANBgkqhkiG9w0BAQUFADA/\n" \
+     "MSQwIgYDVQQKExtEaWdpdGFsIFNpZ25hdHVyZSBUcnVzdCBDby4xFzAVBgNVBAMT\n" \
+     "DkRTVCBSb290IENBIFgzMB4XDTAwMDkzMDIxMTIxOVoXDTIxMDkzMDE0MDExNVow\n" \
+     "PzEkMCIGA1UEChMbRGlnaXRhbCBTaWduYXR1cmUgVHJ1c3QgQ28uMRcwFQYDVQQD\n" \
+     "Ew5EU1QgUm9vdCBDQSBYMzCCASIwDQYJKoZIhvcNAQEBBQADggEPADCCAQoCggEB\n" \
+     "AN+v6ZdQCINXtMxiZfaQguzH0yxrMMpb7NnDfcdAwRgUi+DoM3ZJKuM/IUmTrE4O\n" \
+     "rz5Iy2Xu/NMhD2XSKtkyj4zl93ewEnu1lcCJo6m67XMuegwGMoOifooUMM0RoOEq\n" \
+     "OLl5CjH9UL2AZd+3UWODyOKIYepLYYHsUmu5ouJLGiifSKOeDNoJjj4XLh7dIN9b\n" \
+     "xiqKqy69cK3FCxolkHRyxXtqqzTWMIn/5WgTe1QLyNau7Fqckh49ZLOMxt+/yUFw\n" \
+     "7BZy1SbsOFU5Q9D8/RhcQPGX69Wam40dutolucbY38EVAjqr2m7xPi71XAicPNaD\n" \
+     "aeQQmxkqtilX4+U9m5/wAl0CAwEAAaNCMEAwDwYDVR0TAQH/BAUwAwEB/zAOBgNV\n" \
+     "HQ8BAf8EBAMCAQYwHQYDVR0OBBYEFMSnsaR7LHH62+FLkHX/xBVghYkQMA0GCSqG\n" \
+     "SIb3DQEBBQUAA4IBAQCjGiybFwBcqR7uKGY3Or+Dxz9LwwmglSBd49lZRNI+DT69\n" \
+     "ikugdB/OEIKcdBodfpga3csTS7MgROSR6cz8faXbauX+5v3gTt23ADq1cEmv8uXr\n" \
+     "AvHRAosZy5Q6XkjEGB5YGV8eAlrwDPGxrancWYaLbumR9YbK+rlmM6pZW87ipxZz\n" \
+     "R8srzJmwN0jP41ZL9c8PDHIyh8bwRLtTcm1D9SZImlJnt1ir/md2cXjbDaJWFBM5\n" \
+     "JDGFoqgCWjBH4d1QB7wCCZAA62RjYJsWvIjJEubSfZGL+T0yjWW06XyxV3bqxbYo\n" \
+     "Ob8VZRzI9neWagqNdwvYkQsEjgfbKbYK7p2CNTUQ\n" \
+     "-----END CERTIFICATE-----\n";
 
-//#Bibliotecas MULTITASK
-#include <Arduino.h>
-#include <freertos/FreeRTOS.h>
-#include <freertos/task.h>
-#include "esp32-hal-psram.h"
-#include <esp_system.h>
-#include <esp_task_wdt.h>
-#include "esp32-hal.h"
-
-RTC_DATA_ATTR bool bootTriggered = false;
+//#Bibliotecas 
 
 //#Biblioteca SISTEMAS
-#include "Arduino.h"
+
 #include "time.h"
 #include "sntp.h"
 #include <ArduinoJson.h>
@@ -57,11 +82,10 @@ RTC_DATA_ATTR bool bootTriggered = false;
 #include <stdbool.h>
 #include <stdint.h>
 
-// Crie um objeto client
-WiFiClientSecure client;
 
-// Cria um objeto WebServer
-WebServer server(80);
+
+
+
 
 //##################### Configura IP
 
@@ -71,8 +95,8 @@ IPAddress localIP;
 // Set your Gateway IP address
 IPAddress localGateway;
 //IPAddress localGateway(192, 168, 1, 1); //hardcoded
-IPAddress subnet(255, 255, 255, 0);
-IPAddress primaryDNS(1, 1, 1, 1); //nao nao eh opcional nao, eh obrigatorio demorei mt tempo pra perceber q o relogio e o telegram n tava funcionando por causa do dns
+IPAddress subnet(255, 255, 0, 0);
+IPAddress primaryDNS(8, 8, 8, 8); //nao nao eh opcional nao, eh obrigatorio demorei mt tempo pra perceber q o relogio e o telegram n tava funcionando por causa do dns
 
 
 
@@ -81,13 +105,13 @@ IPAddress primaryDNS(1, 1, 1, 1); //nao nao eh opcional nao, eh obrigatorio demo
 
 //###########################CONFIGURACAO DAS PINAGENS
 
-      int Button1 = 0;  // Pino do botão
-      int Button2 = 4;  // Pino do botão
-      int obj1 = 42;  // Pino do botão
-const int ledPin = 48;     // Pino do LED
-const int relayPin = 1; // Pino GPIO para o relé
-const int relayPin2 = 2; // Pino GPIO para o relé
-const int relayPin3 = 3; // Pino GPIO para o relé
+      int Button1 = 0;  // Pino do bot         o
+      int Button2 = 0;  // Pino do bot         o
+      int obj1 = 0;  // Pino do bot         o
+const int ledPin = 0;     // Pino do LED
+const int relayPin = 0; // Pino GPIO para o rel         
+const int relayPin2 = 0; // Pino GPIO para o rel         
+const int relayPin3 = 0; // Pino GPIO para o rel         
 
 
 const char* host = "santocyber.helioho.st";
@@ -96,15 +120,20 @@ String url6 =       "https://santocyber.helioho.st/granagw/verificaacao.php";
 String urlcredito = "https://santocyber.helioho.st/granagw/apicredito.php";
 String urlpix =     "https://santocyber.helioho.st/granagw/v2/pixapi.php";
 String urlorders =  "https://santocyber.helioho.st/granagw/v2/api_orders.php";
+String serverAddress =  "https://santocyber.helioho.st/granagw/log/receber_dados.php";
+String urlsalvafoto = "https://santocyber.helioho.st/granagw/salvafoto.php";
+
 
 
 bool wifiConnected = false;
 String ssid;
-String password;
+String password ;
 String nomedobot;
 String geo;
+String usuario;
 void saveWifiCredentials(const String &ssid, const String &password, const String &nomedobot, const String &geo);
 void loadWifiCredentials(String &ssid, String &password, String &nomedobot, String &geo);
+void loadfile(String &estado, String &mensagem);
 
 
 //##############CONFIG DOS CONTADORES
@@ -135,9 +164,9 @@ String mensagem;
 int ordervalue = 1;
 
 
-// Variáveis para rastrear o estado do botão e os cliques
-int buttonState = 1;       // Variável para armazenar o estado do botão
-int objState = 0;       // Variável para armazenar o estado do botão
+// Vari         veis para rastrear o estado do bot         o e os cliques
+int buttonState = 1;       // Vari         vel para armazenar o estado do bot         o
+int objState = 0;       // Vari         vel para armazenar o estado do bot         o
 int contaobjfunc = 0;
 bool functionExecuted = false;
 bool functionExecutedX = false;
@@ -153,7 +182,14 @@ int verificacoes = 0;
 
 //###########################################CONFIGURA O LOOP DE UPDATE NO SERVDIOR SQL
 unsigned long previousMillis = 0;
+
+unsigned long previousMillis2 = 0;
+unsigned long previousMillis3 = 0;
+
+
 const long interval = 30000; // Intervalo de 30 segundos
+const long interval60 = 70000; // Intervalo de 30 segundos
+const long interval120 = 120000; // Intervalo de 30 segundos
 
 const long interval2 = 500;  // Intervalo desejado em milissegundos (200ms)
 const long intervalupdate = 30000;  // Intervalo desejado em milissegundos (30s)
@@ -168,19 +204,6 @@ const long  gmtOffset_sec = -10800;
 const int   daylightOffset_sec = 0;
 
 const char* time_zone = "CET-3CEST,M3.5.0,M10.5.0/3";  // TimeZone rule for Europe/Rome including daylight adjustment rules (optional)
-
-String obterHoraAtual() {
-
-    time_t agora = time(nullptr); // Obtemos a hora atual
-    struct tm infoTempo;
-    localtime_r(&agora, &infoTempo); // Converte o tempo em uma estrutura tm
-
-    char buffer[20]; // Buffer para armazenar a hora formatada (HH:MM:SS\0)
-    snprintf(buffer, sizeof(buffer), "%02d:%02d:%02d_%02d/%02d/%04d", infoTempo.tm_hour, infoTempo.tm_min, infoTempo.tm_sec, infoTempo.tm_mday, infoTempo.tm_mon + 1, infoTempo.tm_year + 1900);
-             
-    return String(buffer);
-}
-
 
 void obterHoraEDataAtual() {
     time_t agora = time(nullptr); // Obtemos a hora atual
@@ -198,147 +221,115 @@ void obterHoraEDataAtual() {
 }
 
 
-void setClock() {
-
-  sntp_servermode_dhcp(1);    // (optional)
-  configTime(gmtOffset_sec, daylightOffset_sec, ntpServer1, ntpServer2);
-}
-
-
-
-
-
-
-bool initWiFi() {
-  client.setCACert(caCert);
-  loadWifiCredentials(ssid, password, nomedobot,geo);
-  ssid.trim();
-  password.trim();
-  nomedobot.trim();
-  
-  WiFi.mode(WIFI_STA);
-  
-  if(ssid==""){
-    Serial.println("SSID indefindo");
-    return false;
-  }
-
- // Verifique se o SSID e a senha não estão vazios
-  if (ssid.length() > 0 && password.length() > 0) {
-
-    WiFi.begin(ssid, password);
-    Serial.println("Connecting to WiFi...");
-    Serial.println(ssid.c_str());
-    Serial.println(password.c_str());
-    Serial.println(nomedobot.c_str());
-    Serial.println(geo.c_str());
-    }
-   delay(5000);
-  if (WiFi.status() == WL_CONNECTED) {
-  Serial.println("conectado");
-  WiFi.setHostname(nomedobot.c_str());
-  localIP;
-  localGateway;
-  Serial.println(WiFi.localIP());
-  StateUpdate = "ativo";
-
-
-
- if (!MDNS.begin(nomedobot.c_str())) {
-    Serial.println("Error setting up MDNS responder!");
-    return false;
-  } else {
-    MDNS.addService("http", "tcp", 80);
-    Serial.printf("mDNS responder started '%s'\n", nomedobot.c_str());
-    Serial.print("You can now connect to http://");
-    Serial.print(nomedobot.c_str());
-    Serial.println(".local");   
-
- }
-
-    return true;
-
-  
-  if (!WiFi.config(localIP, localGateway, subnet,primaryDNS)){
-    Serial.println("STA Failed to configure");
-    return false;
-  }
-    } else {
-    Serial.println("nao conectado");
-   return false;
-
-    }
-  }
-
-
 void setup() {
    Serial.begin(115200);
+   Serial.println("Dispositivo GRANA iniciado");
+
+#if (SENSORES == 1)
+
+  // Inicialize o barramento I2C com os pinos SDA e SCL 
+ //   Wire.begin(); // Inicialize a primeira porta I2C  
+    Wire.begin(I2C_MASTER_SDA1, I2C_MASTER_SCL1); // Inicialize a primeira porta I2C
+//  Wire.setClock(100000); 
+
+   if (aht.begin()) {
+    
+   Serial.println("AHT inicializado com sucesso!");
+
+     Serial.println("AHT10/AHT20 Found!");
+  aht_temp = aht.getTemperatureSensor();
+  aht_temp->printSensorDetails();
+
+  aht_humidity = aht.getHumiditySensor();
+  aht_humidity->printSensorDetails();
+
+  }else{
+    Serial.println("Falha ao inicializar o sensor AHT!");
+
+}
+
+   if (bmp.begin()) {
+    
+   Serial.println("BMP inicializado!");
+
+     Serial.println("BMP Found!");
+/* Default settings from datasheet. */
+  bmp.setSampling(Adafruit_BMP280::MODE_NORMAL,     /* Operating Mode. */
+                  Adafruit_BMP280::SAMPLING_X2,     /* Temp. oversampling */
+                  Adafruit_BMP280::SAMPLING_X16,    /* Pressure oversampling */
+                  Adafruit_BMP280::FILTER_X16,      /* Filtering. */
+                  Adafruit_BMP280::STANDBY_MS_500); /* Standby time. */
 
 
-   // Verifica se o boot foi causado pelo watchdog
-  if (bootTriggered) {
-    Serial.println("############Reiniciado pelo RTC watchdog!");
-  }
-  // Configura o RTC watchdog para reiniciar o sistema após 60 segundos
-  esp_task_wdt_init(60, true);
-  // Reinicia o watchdog para evitar reinício imediato
-  esp_task_wdt_reset();
+  }else{
+    Serial.println("Falha ao inicializar o sensor BMP!");
+
+}
+
+#endif
   
-
-  // Inicialize o barramento I2C com os pinos SDA e SCL específicos
-  Wire.begin(8, 9); // Inicializando o barramento I2C com SDA no pino 8 e SCL no pino 9
-  Wire.setClock(100000); 
-  bmp.begin();
-  aht.begin();
-  
-  // Configurar o pino do relé como saída
-  pinMode(Button1, INPUT); // Define o pino do botão como entrada
-  pinMode(Button2, INPUT); // Define o pino do botão como entrada
-  pinMode(obj1, INPUT); // Define o pino do botão como entrada
-  pinMode(ledPin, OUTPUT);   // Define o pino do LED como saída
+  // Configurar o pino do 
+  pinMode(Button1, INPUT); // Define o pino do bot         o como entrada
+  pinMode(Button2, INPUT); // Define o pino do bot         o como entrada
+  pinMode(obj1, INPUT); // Define o pino do bot         o como entrada
+  pinMode(ledPin, OUTPUT);   // Define o pino do LED como sa         da
   pinMode(relayPin, OUTPUT);
   pinMode(relayPin2, OUTPUT);
   pinMode(relayPin3, OUTPUT);
-  digitalWrite(obj1, LOW); // Inicialmente, o relé está desligado
-  digitalWrite(relayPin, LOW); // Inicialmente, o relé está desligado
-  digitalWrite(relayPin2, LOW); // Inicialmente, o relé está desligado
-  digitalWrite(relayPin3, LOW); // Inicialmente, o relé está desligado
+  digitalWrite(obj1, LOW); // Inicialmente, o rel          est          desligado
+  digitalWrite(relayPin, LOW); // Inicialmente, o rel          est          desligado
+  digitalWrite(relayPin2, LOW); // Inicialmente, o rel          est          desligado
+  digitalWrite(relayPin3, LOW); // Inicialmente, o rel          est          desligado
 
 if (!SPIFFS.begin(true)) {
     Serial.println("An error has occurred while mounting SPIFFS");
   }
   Serial.println("SPIFFS mounted successfully");
 
-  if(initWiFi()) {
-  server.on("/", handleMain);
- // server.on("/desligar", handleDesligar);
- // server.on("/ligar", handleLigar);
-  server.begin();
-  Serial.println("Servidor web iniciado");
-  }
 
-else {
-// Iniciar o modo AP
-  WiFi.mode(WIFI_AP);
-  WiFi.softAP("GRANANET-Config"); // Nome da rede AP
-  IPAddress myIP = WiFi.softAPIP();
-  Serial.print("Endereço IP do AP: ");
-  Serial.println(myIP);
-  // Inicializa a biblioteca WiFi
-  WiFi.begin();
 
-    // Inicializa o servidor web
-  server.on("/", handleRoot);
-  server.on("/scan", HTTP_GET, handleScan);
-  server.on("/connect", handleConnect);
- // server.on("/desligar", handleDesligar);
- // server.on("/ligar", handleLigar);
-  server.begin();
-  Serial.println("Servidor web AP iniciado");
+// Carrega as credenciais Wi-Fi e realiza trim nos valores
+loadWifiCredentials(ssid, password, nomedobot, geo, usuario);
+ssid.trim();
+password.trim();
+nomedobot.trim();
 
+// Verifica se tanto o SSID quanto a senha não estão vazios
+if (ssid.length() > 0 && password.length() > 0) {
+    // Tenta conectar-se à rede Wi-Fi
+    WiFi.begin(ssid.c_str(), password.c_str());
+    checkWiFiConnection();
+    Serial.println("Conectando ao WiFi...");
+    Serial.println("SSID: " + ssid);
+    Serial.println("Password: " + password);
+    Serial.println("Nome do bot: " + nomedobot);
+    Serial.println("Geolocalização: " + geo);
+    Serial.println("Usuario: " + usuario);
+    
+    // Aguarda conexão por até 10 segundos
+    unsigned long startTime = millis();
+    while (WiFi.status() != WL_CONNECTED && millis() - startTime < 10000) {
+        delay(500);
+    }
+
+    // Se não conectar, iniciar o modo AP
+    if (WiFi.status() != WL_CONNECTED) {
+        Serial.println("Falha ao reconectar ao Wi-Fi.");
+        startAPMode();
+    } else {
+        // Conexão bem-sucedida, iniciar o servidor web
+        startWebServer();
+    }
+} else {
+    // Se as credenciais estiverem vazias, iniciar o modo AP
+    startAPMode();
 }
 
-  Serial.println("ESP Setup finalizado");
+
+
+    Serial.println("Configuração do ESP finalizada");
+
+
   
   loadfile(estado, mensagem);
   
@@ -349,7 +340,18 @@ else {
 
   // Serial.setDebugOutput(true);
 //#####################################################wifi scan
-  setClock();  
+
+#if (CAMERA == 1)
+
+    // Camera init
+if (!setupCamera())
+  {
+    Serial.println("Camera Setup Failed!");
+  }else{    Serial.println("Camera INICIADA");
+}
+#endif
+
+
 
   }
 
@@ -362,29 +364,83 @@ else {
 
 void loop() {
 
-    // Reinicializa o watchdog a cada loop
-    esp_task_wdt_reset();
-
-
-
     
+    server.handleClient();
 
+  unsigned long currentMillis = millis();
+  unsigned long currentMillis2 = millis();
+    unsigned long currentMillis3 = millis();
+
+
+ // Verifica se já passou o intervalo definido
+  if (currentMillis - previousMillis >= interval) {
+    // Atualiza o tempo de referência
+    previousMillis = currentMillis;
+
+
+    if (WiFi.status() == WL_CONNECTED) {
+    Serial.println("Conectado");
+  // Obtém o tempo atual em milissegundos
 
       telemetria();
-delay(30000);
+      pingando();
+
+    // Por exemplo, você pode imprimir uma mensagem
+    Serial.println("Passaram-se 30 segundos!");
+  }}
+
+
+  
+#if (CAMERA == 1)
+
+
+ // Verifica se já passou o intervalo definido
+  if (currentMillis2 - previousMillis2 >= interval60) {
+    // Atualiza o tempo de referência
+    previousMillis2 = currentMillis2;
+
+
+    if (WiFi.status() == WL_CONNECTED) {
+    Serial.println("Conectado");
+  // Obtém o tempo atual em milissegundos
+
+         capturaimagem();
+
+    // Por exemplo, você pode imprimir uma mensagem
+    Serial.println("Passaram-se 60 segundos!");
+  }}
+#endif
+
+      
+#if (SENSORES == 1)
 
   Serial.println("SENSORES TEMPERATURA");
   Serial.println(readDHTTemperature());
   Serial.println(readDHTHumidity());
   Serial.println(readDHTPressao());
-  
-  Serial.println("VARIAVEIS ARMAZENADA NO SPIFF");
-  Serial.println("ESTADO");
-  Serial.println(estado);
-  Serial.println("MENSAGEM");
-  Serial.println(mensagem);
+  #endif
+ 
+
+// Verifica se já passou o intervalo definido
+  if (currentMillis3 - previousMillis3 >= interval120) {
+    // Atualiza o tempo de referência
+    previousMillis3 = currentMillis3;
+
+
+  checkWiFiConnection();
+
+    // Por exemplo, você pode imprimir uma mensagem
+    Serial.println("Passaram-se 120 segundos!");
+  }
+
+//  Serial.println("VARIAVEIS ARMAZENADA NO SPIFF SALVAS SE DESLIGAR");
+//  Serial.println("ESTADO");
+ // Serial.println(estado);
+ // Serial.println("MENSAGEM");
+ // Serial.println(mensagem);
 
 
 }
+
 
  
